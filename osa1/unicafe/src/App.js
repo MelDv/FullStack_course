044c13cf73489294077635a4f7fd2586985fcd9a
Maaret Dufva <MelDv@users.noreply.github.com>
2022-01-31 19:08:38 +0200

    tehtävä 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -16,16 +16,12 @@ const StatText = (props) => {
   )
 }
 
-const Button = (props) => {
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-}
 
 const App = () => {
   const [clicks, setClicks] = useState({
-    good: 0, neutral: 0, bad: 0, sum: 0
+    good: 0, neutral: 0, bad: 0, sum: 0, average: 0, positives: 0
   })
+
   const header1 = 'give feedback'
   const headerStat = 'statistics'
   const textGood = 'good'
@@ -33,38 +29,50 @@ const App = () => {
   const textBad = 'bad'
   const textAll = 'all'
   const textAv = 'average'
-  const textPos = 'pos'
-
+  const textPos = 'positive'
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [sum, setSum] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positives, setPositives] = useState(0)
 
-  const handGoodClick = () => 
-    setClicks({ ...clicks, good: clicks.good + 1, sum: clicks.sum + 1 })
-
-
-    const handleNeutralClick = () =>
-      setClicks({ ...clicks, neutral: clicks.neutral + 1, sum: clicks.sum + 1 })
-
-
-    const handleBadClick = () =>
-      setClicks({ ...clicks, bad: clicks.bad + 1, sum: clicks.sum + 1 })
-
-    return (
-      <div >
-        <Header title={header1} />
-        <button onClick={handGoodClick}>good</button>
-        <button onClick={handleNeutralClick}>neutral</button>
-        <button onClick={handleBadClick}>bad</button>
-        <Header title={headerStat} />
-        <StatText category={textGood} state={clicks.good} />
-        <StatText category={textNeutral} state={clicks.neutral} />
-        <StatText category={textBad} state={clicks.bad} />
-        <StatText category={textAll} state={clicks.sum} />
-      </div>
-    )
+  const handGoodClick = () => {
+    setGood(good + 1)
+    setSum(sum + 1)
+    setAverage((good - bad) / sum * 1.0)
+    setPositives(good / sum)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setSum(sum + 1)
+    setAverage((good - bad) / sum * 1.0)
+    setPositives(good / sum)
   }
 
-  export default App;
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setSum(sum + 1)
+    setAverage((good - bad) / sum * 1.0)
+    setPositives(good / sum)
+  }
+
+  return (
+    <div >
+      <Header title={header1} />
+      <button onClick={handGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <Header title={headerStat} />
+      <StatText category={textGood} state={good} />
+      <StatText category={textNeutral} state={neutral} />
+      <StatText category={textBad} state={bad} />
+      <StatText category={textAll} state={sum} />
+      <StatText category={textAv} state={average} />
+      <StatText category={textPos} state={positives} />
+    </div>
+  )
+}
+
+export default App;
