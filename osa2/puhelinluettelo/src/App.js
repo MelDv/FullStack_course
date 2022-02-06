b37bc23c3343248940c1bedb8780e06296398d53
Maaret Dufva <MelDv@users.noreply.github.com>
2022-02-06 22:18:21 +0200

    tehtävä 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,13 +3,14 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '000'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -34,6 +35,25 @@ const App = () => {
     setNewNumber('')
   }
 
+  const doFiltering = (event) => {
+    event.preventDefault()
+    let filtered = []
+
+    if (newFilter === '') {
+      document.location.reload()
+    }
+    persons.map(person => {
+      if (person.name.toLowerCase().includes(newFilter.toLowerCase())) {
+        filtered.push(person)
+      }
+    })
+    if (filtered.length === 0) {
+      window.alert(`no name contains ${newFilter}`)
+      document.location.reload()
+    }
+    setPersons(filtered)
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -42,9 +62,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const showAllPersons = () => {
+    document.location.reload()
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <button onClick={showAllPersons}>show all</button>
+      </div>
+      <form onSubmit={doFiltering}>
+        <div>
+          filter shown with <input
+            value={newFilter}
+            onChange={handleFilterChange} />
+        </div>
+        <div>
+          <button type="submit">filter</button>
+        </div>
+      </form>
       <form onSubmit={addName}>
         <div>
           name: <input
@@ -67,7 +108,6 @@ const App = () => {
       </ul>
     </div>
   )
-
 }
 
 export default App
